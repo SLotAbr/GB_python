b9f5032254ad2547dba81c9342e3e810e3605445
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2021-02-02 19:32:33 +0300

    simplified task-2

--- task-2.py
+++ task-2.py
@@ -1,12 +1,3 @@
-def proper_display(digit):
-	string = str(digit)
-	if len(string)==1:
-		return '0'+string
-	else:
-		return string
-
-
 n = int(input('Please, enter the number of seconds:\n'))
 hh, mm, ss = n // 3600, (n // 60) % 60, n % 60
-hh, mm, ss = proper_display(hh), proper_display(mm), proper_display(ss)
-print('{}:{}:{}'.format(hh, mm, ss))
+print('{:>02}:{:>02}:{:>02}'.format(hh, mm, ss))
